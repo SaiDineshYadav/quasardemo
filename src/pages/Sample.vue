74edc3926153c99c16f2f58f07c8afd3925c70be
SaiDineshYadav <boddusaidineshyadav@gmail.com>
2021-02-12 12:03:52 +0530

    Dinesh added Quasar demo proj

---
+++ src/pages/Sample.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <h3>Hello Sample </h3>
+        <q-banner rounded class="bg-purple-8 text-white">
+
+      We can't find your saved recipes until you sign in.
+
+      <template v-slot:action>
+        <q-btn flat color="white" label="Continue as a Guest" />
+        <q-btn flat color="white" label="Sign in" />
+      </template>
+    </q-banner>
+      <div>
+      <q-chip icon="event">Add to calendar</q-chip>
+      <q-chip icon="bookmark">Bookmark</q-chip>
+      <q-chip icon="alarm" label="Set alarm" />
+      <q-chip class="glossy" icon="directions">Get directions</q-chip>
+    </div>
+    <div>
+      <q-chip color="primary" text-color="white" icon="event">
+        Add to calendar
+      </q-chip>
+      <q-chip color="teal" text-color="white" icon="bookmark">
+        Bookmark
+      </q-chip>
+      <q-chip class="glossy" color="orange" text-color="white" icon-right="star">
+        Star
+      </q-chip>
+      <q-chip color="red" text-color="white" icon="alarm" label="Set alarm" />
+      <q-chip color="deep-orange" text-color="white" icon="directions">
+        Get directions
+      </q-chip>
+      <q-chip>
+        <q-avatar icon="bookmark" color="red" text-color="white" />
+        Bookmark
+      </q-chip>
+      <q-chip>
+        <q-avatar color="red" text-color="white">50</q-avatar>
+        Emails
+      </q-chip>
+      <q-chip>
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+        </q-avatar>
+        John
+      </q-chip>
+    </div>
+      <div class="q-pa-md q-gutter-sm">
+    <q-btn color="white" text-color="black" label="Standard" />
+    <q-btn color="primary" label="Primary" />
+    <q-btn color="secondary" label="Secondary" />
+    <q-btn color="amber" glossy label="Amber" />
+    <q-btn color="brown-5" label="Brown 5" />
+    <q-btn color="deep-orange" glossy label="Deep Orange" />
+    <q-btn color="purple" label="Purple" />
+    <q-btn color="black" label="Black" />
+  </div>
+    </div>
+</template>
